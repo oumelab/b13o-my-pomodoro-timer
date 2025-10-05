52b68185476fcefd256e548c588401110a6a1a69
oume-mini <oumelab@pt.rdy.jp>
2025-10-06 05:39:10 +0900

    Update Vite config to set base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "REPOSITORY_NAME" : "./",
+  base: process.env.GITHUB_PAGES ? "b13o-my-pomodoro-timer" : "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
